1e5b50cd8b21e5d2b1c41bf256b581239010a1b4
williamlin0518 <illiamw81@gmail.com>
2024-01-25 00:54:23 +0800

    add login

---
+++ client/babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    presets: ['react-app'],
+    plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  };
+  
--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+import AppRoot from "./AppRoot";
+import AppLoader from "./AppLoader";
+
 import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
+function App(){
 
-
-function App() {
   return (
     <div>
       <SignedOut>
         <SignInButton />
         <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-              <Link to="/">Home</Link>
-              <Link to="/otherpage">Other Page</Link>
-            </header>
-            <div>
-              {/* <Route exact path="/" component={Fib} /> */}
-              <Route path="/otherpage" component={OtherPage} />
-            </div>
-          </div>
-        </Router>
       </SignedOut>
       <SignedIn>
         <SignOutButton afterSignOutUrl="/" />
         <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-              <Link to="/">Home</Link>
-              <Link to="/otherpage">Other Page</Link>
-            </header>
-            <div>
-              {/* <Route exact path="/" component={Fib} /> */}
-              <Route path="/otherpage" component={OtherPage} />
-            </div>
-          </div>
-        </Router>
+        <AppRoot>
+          <AppLoader />
+        </AppRoot>
       </SignedIn>
     </div>
-    
-  );
+  )
 }
 
 export default App;
