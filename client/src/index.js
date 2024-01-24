fb7daf3aca3f326a219dbf6717cb259fc7c97cf2
williamlin0518 <illiamw81@gmail.com>
2024-01-25 02:18:08 +0800

    log

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_test_Y2hhbXBpb24tY2FyaWJvdS04NS5jbGVyay5hY2NvdW50cy5kZXYk"
 console.log(PUBLISHABLE_KEY)
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
