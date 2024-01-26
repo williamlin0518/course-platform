7a475e1b9d2977267a25a16772e1dee143c0935d
williamlin0518 <illiamw81@gmail.com>
2024-01-26 17:13:26 +0800

    in/out

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,6 @@ import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-r
 function App(){
   return (
     <div>
-      
-      {/* <SignedOut>
-        <SignInButton />
-        <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
-
-        <AppRoot>
-          <AppLoader />
-        </AppRoot>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton afterSignOutUrl="/" />
-        <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
-        <AppRoot>
-          <AppLoader />
-        </AppRoot>
-      </SignedIn> */}
-
       <AppRoot>
           <AppLoader />
       </AppRoot>
--- client/src/comps/SignInPage.js
+++ client/src/comps/SignInPage.js
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/clerk-react";
  
 const SignInPage = () => (
-  <SignIn path="/login1" routing="login" signUpUrl="/sign-up" />
+  <SignIn path="/login" routing="login" signUpUrl="/sign-up" />
 );
  
 export default SignInPage;
